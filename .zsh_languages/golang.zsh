1a60e0ba4befce207432774c34f57bc43a18e503
TMK04 <floer.learner.01@gmail.com>
2024-06-21 11:19:43 +0800

    golang: follow go-nv/goenv INSTALL.md#installation

--- .zsh_languages/golang.zsh
+++ .zsh_languages/golang.zsh
@@ -9,28 +9,9 @@ load_golang() {
 
   export GOENV_ROOT="$HOME/.goenv"
   export PATH="$GOENV_ROOT/bin:$PATH"
-  export GOENV_SHELL=zsh
-  
-  if [ "${PATH#*$GOENV_ROOT/shims}" = "${PATH}" ]; then
-    export PATH="$PATH:$GOENV_ROOT/shims"
-  fi
-  source "$HOME/.goenv/completions/goenv.zsh"
-  command goenv rehash 2>/dev/null
-  goenv() {
-    local command
-    command="$1"
-    if [ "$#" -gt 0 ]; then
-      shift
-    fi
-
-    case "$command" in
-    rehash|shell)
-      eval "$(goenv "sh-$command" "$@")";;
-    *)
-      command goenv "$command" "$@";;
-    esac
-  }
-  goenv rehash --only-manage-paths
+  eval "$(goenv init -)"
+  export PATH="$GOROOT/bin:$PATH"
+  export PATH="$PATH:$GOPATH/bin"
 
   unfunction load_golang
 }
