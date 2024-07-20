ff1eb73a80e92ba147d1d6ff8cd2977c98529f09
TMK04 <floer.learner.01@gmail.com>
2024-07-21 05:36:49 +0800

    setup.zsh: standardize depth=1 & ZDOTDIR

--- setup.zsh
+++ setup.zsh
@@ -1,15 +1,15 @@
-cp "$HOME/.ssh/local-config.example" "$HOME/.ssh/local-config"
+cp "${ZDOTDIR:-$HOME}/.ssh/local-config.example" "${ZDOTDIR:-$HOME}/.ssh/local-config"
 
 brew bundle install
 
 git clone --depth=1 "https://github.com/mattmc3/antidote.git" ${ZDOTDIR:-$HOME}/.antidote
-git clone "https://github.com/go-nv/goenv" "$HOME/.goenv"
-git clone "https://github.com/nvm-sh/nvm" "$HOME/.nvm"
+git clone --depth=1 "https://github.com/go-nv/goenv" "${ZDOTDIR:-$HOME}/.goenv"
+git clone --depth=1 "https://github.com/nvm-sh/nvm" "${ZDOTDIR:-$HOME}/.nvm"
 
-git clone "https://github.com/ethrgeist/git-credential-1password" "$HOME/.git-credential-1password" && \
-  sudo go build -C "$HOME/.git-credential-1password" -o "/usr/local/bin/git-credential-1password" && \
-rm -rf "$HOME/.git-credential-1password"
+git clone --depth=1 "https://github.com/ethrgeist/git-credential-1password" "${ZDOTDIR:-$HOME}/.git-credential-1password" && \
+  sudo go build -C "${ZDOTDIR:-$HOME}/.git-credential-1password" -o "/usr/local/bin/git-credential-1password" && \
+rm -rf "${ZDOTDIR:-$HOME}/.git-credential-1password"
 
-chmod -R 755 "$HOME/.gitfuncs"
-chmod -R 755 "$HOME/.zalias"
-chmod -R 755 "$HOME/.zfuncs"
+chmod -R 755 "${ZDOTDIR:-$HOME}/.gitfuncs"
+chmod -R 755 "${ZDOTDIR:-$HOME}/.zalias"
+chmod -R 755 "${ZDOTDIR:-$HOME}/.zfuncs"
